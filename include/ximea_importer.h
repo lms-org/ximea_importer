6d507ae54a364ad7f5a2b58a5c532d51233c7c48
phibedy <philipp.schmette@gmx.de>
2016-06-09 11:39:14 +0200

    added xi-example, (not tested yet)

--- include/ximea_importer.h
+++ include/ximea_importer.h
@@ -2,6 +2,12 @@
 #define XIMEA_IMPORTER_H
 
 #include <lms/module.h>
+#ifdef WIN32
+#include "xiApi.h"       // Windows
+#else
+#include <m3api/xiApi.h> // Linux, OSX
+#endif
+#include <memory.h>
 
 /**
  * @brief LMS module ximea_importer
@@ -11,6 +17,12 @@ public:
     bool initialize() override;
     bool deinitialize() override;
     bool cycle() override;
+
+private:
+
+HANDLE xiH;
+XI_RETURN stat;
+XI_IMG image;
 };
 
 #endif // XIMEA_IMPORTER_H
--- src/ximea_importer.cpp
+++ src/ximea_importer.cpp
@@ -1,13 +1,53 @@
 #include "ximea_importer.h"
 
+
+#define HandleResult(res,place) if (res!=XI_OK) {printf("Error after %s (%d)",place,res);}
+
+
+//TODO https://www.ximea.com/support/wiki/apis/XiApi_Manual
+//Taken from the xiExample
 bool XimeaImporter::initialize() {
+    // image buffer
+        memset(&image,0,sizeof(image));
+        image.size = sizeof(XI_IMG);
+
+        // Sample for XIMEA API V4.05
+        HANDLE xiH = NULL;
+        XI_RETURN stat = XI_OK;
+
+        // Retrieving a handle to the camera device
+        printf("Opening first camera...\n");
+        stat = xiOpenDevice(0, &xiH);
+        HandleResult(stat,"xiOpenDevice");
+
+        // Setting "exposure" parameter (10ms=10000us)
+        stat = xiSetParamInt(xiH, XI_PRM_EXPOSURE, 10000);
+        HandleResult(stat,"xiSetParam (exposure set)");
+
+        // Note:
+        // The default parameters of each camera might be different in different API versions
+        // In order to ensure that your application will have camera in expected state,
+        // please set all parameters expected by your application to required value.
+
+        printf("Starting acquisition...\n");
+        stat = xiStartAcquisition(xiH);
+        HandleResult(stat,"xiStartAcquisition");
     return true;
 }
 
 bool XimeaImporter::deinitialize() {
+    printf("Stopping acquisition...\n");
+    xiStopAcquisition(xiH);
+    xiCloseDevice(xiH);
     return true;
 }
 
 bool XimeaImporter::cycle() {
+
+    // getting image from camera
+    stat = xiGetImage(xiH, 5000, &image);
+    HandleResult(stat,"xiGetImage");
+    unsigned char pixel = *(unsigned char*)image.bp;
+    printf("Image %d (%dx%d) received from camera. First pixel value: %d\n", cycleCounter(), (int)image.width, (int)image.height, pixel);
     return true;
 }
