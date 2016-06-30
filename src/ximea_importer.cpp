269f9fdd7d7f390f2dd6731c0a1fdb6c024c4dff
phibedy <philipp.schmette@gmx.de>
2016-06-30 21:10:22 +0200

    first working version

--- src/ximea_importer.cpp
+++ src/ximea_importer.cpp
@@ -9,29 +9,30 @@
 bool XimeaImporter::initialize() {
     myImage = writeChannel<lms::imaging::Image>("IMAGE");
     // image buffer
-        memset(&image,0,sizeof(image));
-        image.size = sizeof(XI_IMG);
+    memset(&image,0,sizeof(image));
+    image.size = sizeof(XI_IMG);
 
-        // Sample for XIMEA API V4.05
-        XI_RETURN stat = XI_OK;
+    // Sample for XIMEA API V4.05
+    XI_RETURN stat = XI_OK;
 
-        // Retrieving a handle to the camera device
-        printf("Opening first camera...\n");
-        stat = xiOpenDevice(0, &xiH);
-        HandleResult(stat,"xiOpenDevice");
+    // Retrieving a handle to the camera device
+    printf("Opening first camera...\n");
+    stat = xiOpenDevice(0, &xiH);
+    HandleResult(stat,"xiOpenDevice");
 
-        // Setting "exposure" parameter (10ms=10000us)
-        stat = xiSetParamInt(xiH, XI_PRM_EXPOSURE, 10000);
-        HandleResult(stat,"xiSetParam (exposure set)");
+    // Setting "exposure" parameter (10ms=10000us)
+    stat = xiSetParamInt(xiH, XI_PRM_EXPOSURE, 10000);
+    HandleResult(stat,"xiSetParam (exposure set)");
+    stat = xiSetParamInt(xiH,XI_PRM_IMAGE_DATA_FORMAT,XI_MONO8);
+    HandleResult(stat,"xiSetParam (XI_PRM_IMAGE_DATA_FORMAT set)");
+    stat = xiSetParamInt(xiH,XI_PRM_AUTO_WB,1);
+    HandleResult(stat,"xiSetParam (XI_PRM_AUTO_WB set)");
+    stat = xiSetParamInt(xiH,XI_PRM_FRAMERATE,100);
+    HandleResult(stat,"xiSetParam (XI_PRM_FRAMERATE set)");
 
-        // Note:
-        // The default parameters of each camera might be different in different API versions
-        // In order to ensure that your application will have camera in expected state,
-        // please set all parameters expected by your application to required value.
-
-        printf("Starting acquisition...\n");
-        stat = xiStartAcquisition(xiH);
-        HandleResult(stat,"xiStartAcquisition");
+    printf("Starting acquisition...\n");
+    stat = xiStartAcquisition(xiH);
+    HandleResult(stat,"xiStartAcquisition");
     return true;
 }
 
@@ -43,11 +44,12 @@ bool XimeaImporter::deinitialize() {
 }
 
 bool XimeaImporter::cycle() {
-
     // getting image from camera
     stat = xiGetImage(xiH, 5000, &image);
     HandleResult(stat,"xiGetImage");
-    unsigned char pixel = *(unsigned char*)image.bp;
-    printf("Image %d (%dx%d) received from camera. First pixel value: %d\n", cycleCounter(), (int)image.width, (int)image.height, pixel);
+
+    myImage->resize(image.width,image.height,lms::imaging::Format::GREY);
+    myImage->fill(255);
+    memcpy(myImage->data(),image.bp,myImage->size());
     return true;
 }
